622ad0d4c762af8547cd313fd173b70c4f044865
dawica <dwcampman@gmail.com>
2025-02-11 15:48:08 -0500

    Added Buffer.java

---
+++ Buffer.java
@@ -0,0 +1,15 @@
+/**
+ * Stores pages in memory
+ * Authors:
+ */
+public class Buffer {
+    int size;
+
+    /**
+     * Creates a buffer for storing pages
+     * @param bufferSize The size of the buffer
+     */
+    public Buffer(int bufferSize) {
+        size = bufferSize;
+    }
+}
