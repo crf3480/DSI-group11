b98eeabd750e5e50995349c7201d249df7fd8db6
dawica <dwcampman@gmail.com>
2025-02-11 15:38:17 -0500

    Renamed Parser to Main and added argument parsing

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Validate CLI arguments
+        if (args.length < 3) {
+            System.out.println("Usage: java -jar Parser.jar <file> <page size> <buffer size>");
+            System.exit(1);
+        }
+        String dbLocation = args[0];
+        try {
+            int pageSize = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Invalid page size: `" + args[1] + "`");
+        }
+        try {
+            int bufferSize = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Invalid buffer size: `" + args[2] + "`");
+        }
+    }
+}
--- Parser.java
+++
@@ -1,8 +0,0 @@
-public class Parser {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        String dbLocation = args[0];
-        String pageSize = args[1];
-        String bufferSize = args[2];
-    }
-}
