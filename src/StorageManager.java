b88ca76230c1b6ee5932330321529be423ed10a8
V4RR1K <gclynskey@outlook.com>
2025-02-18 15:53:03 -0500

    Greg: Adding a storage manager class and roughing out stated functionality. All subject to change, just reading off the sheet.

---
+++ src/StorageManager.java
@@ -0,0 +1,27 @@
+public class StorageManager {
+
+    public boolean getByPrimaryKey(int id){
+        return false;
+    }
+
+    // Unsure about this one...
+    public boolean pageByTableAndPageNum(int tableNum, int pageNum){
+        return false;
+    }
+
+    public boolean getAllInTable(int tableNum){
+        return false;
+    }
+
+    public boolean insertRecord(){ // Param for record?
+        return false;
+    }
+
+    public boolean deleteByPrimaryKey(int id){
+        return false;
+    }
+
+    public boolean updateByPrimaryKey(int id){
+        return false;
+    }
+}
