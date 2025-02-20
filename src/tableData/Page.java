8bbd8d050f82eb038406a2a9bc218d3d6218aeca
V4RR1K <gclynskey@outlook.com>
2025-02-20 17:38:29 -0500

    Greg: decode and encode page/record are done aside from adding an end condition. Needs testing once storage manager is implemented.

--- src/tableData/Page.java
+++ src/tableData/Page.java
@@ -9,7 +9,7 @@ import java.io.*;
  */
 public class Page {
 
-    public int pageNumber;
+    public int pageNumber;  // Increase this to a long if you need more than 127 pages
     public ArrayList<Record> records;
 
     /**
@@ -22,9 +22,26 @@ public class Page {
     }
 
     /**
+     * Creates a page object given page number, expecting the records to be populated in decode method
+     * @param pageNumber page num of given page
+     */
+    public Page(int pageNumber) {
+        this.pageNumber = pageNumber;
+        this.records = new ArrayList<>();
+    }
+
+    /**
+     * Used in page splitting in order to add a new page number to a given page
+     * @param newPageNumber to be added
+     */
+    public void UpdatePageNumber(int newPageNumber) {
+        this.pageNumber = newPageNumber;
+    }
+
+    /**
      * Encodes the given page to the end of the page file
      * Encodes to data to binary in the form P#, Records, EOP
-     * ONLY WORKS IS P# between (0, 127) -> Increase page num to a long to fix
+     * ONLY WORKS IS P# between (0, 127)
      * @param ts the table schema, used to get size of attr/record
      */
     public ByteArrayOutputStream encodePage(TableSchema ts) throws IOException {
@@ -36,6 +53,9 @@ public class Page {
         for (Record record : records) {
             bs.write(record.encodeRecord(ts).toByteArray()); // Encoded record
         }
+
+        // TODO: Do something so signal all records have been encoded
+
         return bs;
     }
 
@@ -43,10 +63,21 @@ public class Page {
      * Decodes the page given a chunk of binary in the format:
      * P#, Records, EOP
      */
-    public void decodePage(TableSchema ts){
+    public void decodePage(TableSchema ts, byte[] pageBytes) throws IOException {
+        ByteArrayInputStream bs = new ByteArrayInputStream(pageBytes);
+        DataInputStream ds = new DataInputStream(bs);
 
+        pageNumber = ds.readInt();
 
+        int recordSize = getRecordSize(ts);
 
+        // TODO: read until you hit the end condition
+        while (true){ // Reads infinitely, change this once we have a set end condition
+            byte[] singleRecord = ds.readNBytes(recordSize);
+            Record record = new Record();
+            record.decodeRecord(ts, singleRecord);
+            records.add(record);
+        }
     }
 
     private int getRecordSize(TableSchema ts) {
