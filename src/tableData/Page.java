c7d55853a9663b199aefd8791234d0b4eb0615cb
V4RR1K <gclynskey@outlook.com>
2025-02-20 10:15:12 -0500

    Greg: Stubbed and commented page and page manager files

--- src/tableData/Page.java
+++ src/tableData/Page.java
@@ -1,6 +1,7 @@
 package tableData;
 
 import java.util.ArrayList;
+import java.io.*;
 
 /**
  * Represents a page of a Table
@@ -12,9 +13,30 @@ public class Page {
     public ArrayList<Record> records;
 
     /**
-     * Creates a Catalog.Page
+     * Creates a page object, given the page number and records
+     * @param pageNumber Number of given page
      */
-    public Page() {
+    public Page(int pageNumber, ArrayList<Record> records) {
+        this.pageNumber = pageNumber;
+        this.records = records;
+    }
+
+    /**
+     * Encodes the given page to the end of the page file
+     * Encodes to data to binary in the form P#, Records, EOP
+     * If the size of records exceeds the page size, a new page is created,
+     * and the data is split
+     * ONLY WORKS IS P# between (0, 127)
+     */
+    public void encodePage(){
+        byte b_PageNum = (byte) pageNumber;
+    }
+
+    /**
+     * Decodes the page given a chunk of binary in the format:
+     * P#, Records, EOP
+     */
+    public void decodePage(){
 
     }
 }
---
+++ src/tableData/PageManager.java
@@ -0,0 +1,24 @@
+package tableData;
+
+import java.util.ArrayList;
+
+public class PageManager {
+    ArrayList<Page> pages;
+    String fileName;
+
+    /**
+     * Reads in a data file and initializes all the pages.
+     * If there is no file it creates one
+     * @param fileName Name of data file (TableName.bin)
+     */
+    public PageManager(String fileName) {
+
+    }
+
+    /**
+     * Saves the data of the page to the file
+     */
+    public void save(){
+
+    }
+}
