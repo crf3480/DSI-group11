1a55d0656d5ecf37894a39f89c26cf9cfb988280
D. William Campman <dwcampman@gmail.com>
2025-02-21 13:01:41 -0500

    Added toString method for AttributeType

--- src/tableData/AttributeType.java
+++ src/tableData/AttributeType.java
@@ -1,5 +1,17 @@
 package tableData;
 
 public enum AttributeType {
-    INT, DOUBLE, CHAR, VARCHAR, BOOLEAN
+    INT, DOUBLE, CHAR, VARCHAR, BOOLEAN;
+
+    public AttributeType fromString(String typeName) throws IllegalArgumentException {
+        typeName = typeName.toUpperCase();
+        return switch (typeName) {
+            case "INT" -> INT;
+            case "DOUBLE" -> DOUBLE;
+            case "CHAR" -> CHAR;
+            case "VARCHAR" -> VARCHAR;
+            case "BOOLEAN" -> BOOLEAN;
+            default -> throw new IllegalArgumentException("Invalid attribute type: " + typeName);
+        };
+    }
 }
