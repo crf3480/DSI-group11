0cfedb8188f8e20968367d7e32f27ff7084743c7
soweeby <rgnunna@gmail.com>
2025-02-21 17:53:31 -0500

    correct params for insertRecord in StorageManager

--- src/components/StorageManager.java
+++ src/components/StorageManager.java
@@ -87,7 +87,7 @@ public class StorageManager {
         }
 
 
-    public boolean insertRecord(String tableName, ArrayList<Object> values){
+    public boolean insertRecord(String tableName, ArrayList<ArrayList<Object>> values){
         return false;
     }
 
