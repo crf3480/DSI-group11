d236031ae4b61f25364c181bf909f252a29bb6c1
D. William Campman <dwcampman@gmail.com>
2025-02-21 10:39:20 -0500

    Converted Page to storing its data as a byte array - Page's internal data structure is now a byte array, which it encodes and decodes to an ArrayList of Records when providing data - Removed Record's encode/decode method and moved that functionality into Page, since variable record sizes mean you can't know ahead of time where the boundaries between records is.

--- src/tableData/Page.java
+++ src/tableData/Page.java
@@ -2,6 +2,8 @@ package tableData;
 
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Represents a page of a Table
@@ -9,85 +11,154 @@ import java.io.*;
  */
 public class Page {
 
-    public int pageNumber;  // Increase this to a long if you need more than 127 pages
-    public ArrayList<Record> records;
+    private TableSchema tableSchema;
+    public int pageNumber;
+    public byte[] recordData;
+    private int numRecords;
 
     /**
-     * Creates a page object, given the page number and records
-     * @param pageNumber Number of given page
+     * Creates a page object from a Page data byte array
+     * @param pageData The byte array of page data
+     * @param tableSchema The schema of the data in this page
      */
-    public Page(int pageNumber, ArrayList<Record> records) {
-        this.pageNumber = pageNumber;
-        this.records = records;
+    public Page(byte[] pageData, TableSchema tableSchema) throws IOException {
+        this.tableSchema = tableSchema;
+
+        ByteArrayInputStream inStream = new ByteArrayInputStream(pageData);
+        DataInputStream in = new DataInputStream(inStream);
+        pageNumber = in.readInt();
+        numRecords = in.readInt();
+        recordData = new byte[pageData.length - (Integer.SIZE * 2)];
+        in.readFully(recordData);  // Read page data into the recordData array
     }
 
     /**
-     * Creates a page object given page number, expecting the records to be populated in decode method
+     * Creates an empty page with a given page number
+     * @param pageNumber The number of the page
+     * @param tableSchema The schema of the records stored in this page
+     * @param pageSize The page size in bytes
      */
-    public Page() {
-        this.records = new ArrayList<>();
+    public Page(int pageNumber, TableSchema tableSchema, int pageSize) {
+        this.pageNumber = pageNumber;
+        this.tableSchema = tableSchema;
+        recordData = new byte[pageSize - (Integer.BYTES * 2)];  // Leave room for the page # and record count
+        numRecords = 0;
+    }
+
+    /**
+     * Inserts a record into the page
+     * @param record The record being inserted into the page
+     * @return `true` if the record was inserted. `false` if the record could not be inserted because it is full
+     * @throws IOException Encountered a problem when converting records to or from binary
+     */
+    public boolean insertRecord(Record record) throws IOException {
+        ArrayList<Record> records = getRecords();
+        records.add(record);
+        numRecords += 1;
+        byte[] encoded = encodeRecords(records);
+        if (encoded.length > recordData.length) {
+            return false;
+        } else if (encoded.length < recordData.length) {
+            throw new IOException("Invalid page size found when encoding records");
+        }
+        recordData = encoded;
+        return true;
     }
 
     /**
      * Used in page splitting in order to add a new page number to a given page
      * @param newPageNumber to be added
      */
-    public void UpdatePageNumber(int newPageNumber) {
+    public void updatePageNumber(int newPageNumber) {
         this.pageNumber = newPageNumber;
     }
 
     /**
-     * Encodes the given page to the end of the page file
-     * Encodes to data to binary in the form P#, Records, EOP
-     * ONLY WORKS IS P# between (0, 127)
-     * @param ts the table schema, used to get size of attr/record
+     * Gets the list of records in the page
+     * @return The list of records stored in the page
      */
-    public ByteArrayOutputStream encodePage(TableSchema ts) throws IOException {
+    public ArrayList<Record> getRecords() throws IOException {
+        ArrayList<Record> records = new ArrayList<>(numRecords);
+        ByteArrayInputStream inStream = new ByteArrayInputStream(recordData);
+        DataInputStream in = new DataInputStream(inStream);
+        for (int i = 0; i < numRecords; i++) {
+            ArrayList<Object> recordAttr = new ArrayList<>();
+            for (Attribute attr : tableSchema.attributes) {
+                switch (attr.type) {
+                    case INT:
+                        recordAttr.add(in.readInt());
+                        break;
+                    case DOUBLE:
+                        recordAttr.add(in.readDouble());
+                        break;
+                    case BOOLEAN:
+                        recordAttr.add(in.readBoolean());
+                    case CHAR:
+                    case VARCHAR:
+                        recordAttr.add(in.readUTF());
+                        break;
+                    default:
+                        throw new IOException("Invalid attribute type: " + attr.type);
+                }
+            }
+            records.add(new Record(recordAttr));
+        }
+        return records;
+    }
 
-        System.out.println("Encoding Page: " + pageNumber + " | Total Records: " + records.size());
+    /**
+     * Converts a collection of records into an array of bytes. This array will be at minimum the size of the page,
+     * but may be longer if the list of records cannot fit in the page.
+     * @param records The records being encoded
+     * @return An array of bytes containing the data of the records
+     * @throws IOException If there is an error encoding the data
+     */
+    private byte[] encodeRecords(Collection<Record> records) throws IOException {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream(recordData.length);
+        DataOutputStream out = new DataOutputStream(outStream);
+        for (Record record : records) {
+            for (int i = 0; i < tableSchema.attributes.size(); i++) {
+                Attribute attr = tableSchema.attributes.get(i);
+                Object value = record.rowData.get(i);
+                switch (attr.type) {
+                    case INT:
+                        out.writeInt((Integer) value);
+                        break;
+                    case DOUBLE:
+                        out.writeDouble((Double) value);
+                        break;
+                    case BOOLEAN:
+                        out.writeBoolean((Boolean) value);
+                        break;
+                    case CHAR:
+                    case VARCHAR:
+                        out.writeUTF((String) value);
+                        break;
+                    default:
+                        throw new IOException("Invalid attribute type: " + attr.type);
+                }
+            }
+        }
+        return outStream.toByteArray();
+    }
+
+    /**
+     * Encodes the given page to the end of the page file. Encodes to data to binary starting with the
+     * page num, number of records, and then the record data. The array will always be the length of
+     * the page size, so any space not taken up by records will contain indeterminate data.
+     * @return A byte array containing the fully encoded Page
+     */
+    public byte[] encodePage() throws IOException {
+
+        System.out.println("Encoding Page: " + pageNumber + " | Total Records: " + numRecords);
 
         ByteArrayOutputStream bs = new ByteArrayOutputStream();
-        bs.write(pageNumber); // Writes the page number first as an int
-        bs.write(records.size());
-        for (Record record : records) {
-            bs.write(record.encodeRecord(ts).toByteArray()); // Encoded record
-        }
+        DataOutputStream out = new DataOutputStream(bs);
+        out.writeInt(pageNumber); // Writes the page number first
+        out.writeInt(numRecords); // Writes the number of records
+        out.write(recordData);    // Writes the record data
 
-        return bs;
+        return bs.toByteArray();
     }
 
-    /**
-     * Decodes a page starting with P#, NumRecords, Content
-     * @param ts table schema for the page
-     * @param pageBytes bytes for given page
-     */
-    public void decodePage(TableSchema ts, byte[] pageBytes) throws IOException {
-        ByteArrayInputStream bs = new ByteArrayInputStream(pageBytes);
-        DataInputStream ds = new DataInputStream(bs);
-
-        this.pageNumber = ds.readInt();
-
-        int recordCount = ds.readInt();
-        int recordSize = getRecordSize(ts);
-
-        for (int i = 0; i < recordCount; i++){
-            byte[] singleRecord = ds.readNBytes(recordSize);
-            Record record = new Record();
-            record.decodeRecord(ts, singleRecord);
-            records.add(record);
-        }
-    }
-
-    /**
-     * Gets the max size of a given record
-     * @param ts table schema for the attribute list
-     * @return size in bytes for the given record
-     */
-    private int getRecordSize(TableSchema ts) {
-        int size = 0;
-        for (Attribute attr : ts.attributes) {
-            size += attr.length;
-        }
-        return size;
-    }
 }
--- src/tableData/Record.java
+++ src/tableData/Record.java
@@ -1,16 +1,11 @@
 package tableData;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-
-import tableData.AttributeType;
-
-import static tableData.AttributeType.*;
 
 public class Record {
 
-    // pass the table class the record is from
+    /// pass the table class the record is from
     ArrayList<Object> rowData;
 
     /**
@@ -21,83 +16,11 @@ public class Record {
         this.rowData = rowData;
     }
 
-    // Initializer for no row data, use the decode method to populate
+    /**
+     * Initializer for no row data, use the decode method to populate
+     */
     public Record() {
         this.rowData = new ArrayList<>();
-    }
-
-    /**
-     * Encodes a single record into a byte array output stream, assumes record and attr counts are equal
-     * @param ts given table schema to pull attr sizes from
-     * @return ByteArrayOutputStream that can be converted to a byte array
-     * @throws IOException if there's an issue with a write operation
-     */
-    public ByteArrayOutputStream encodeRecord(TableSchema ts) throws IOException {
-        ByteArrayOutputStream bs = new ByteArrayOutputStream();
-        DataOutputStream ds = new DataOutputStream(bs);
-        for (int i = 0; i < ts.attributes.size(); i++) {
-            AttributeType type = ts.attributes.get(i).type;
-            Object value = rowData.get(i);
-            switch (type) {
-                case INT:
-                    ds.writeInt((int)rowData.get(i));
-                    break;
-                case DOUBLE:
-                    ds.writeDouble((double) value);
-                    break;
-                case BOOLEAN:
-                    ds.writeBoolean((boolean) value);
-                    break;
-                case CHAR:
-                case VARCHAR:
-                    byte[] stringBytes = ((String) value).getBytes(StandardCharsets.UTF_8);
-                    ds.write(stringBytes);
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unsupported attribute type: " + type);
-            }
-
-        }
-        ds.flush();
-        return bs;
-    }
-
-    /**
-     * Populates an empty record given a set of bytes the size of the record
-     * @param ts given table schema
-     * @param recordBytes record stored as binary
-     * @throws IOException if there is an issue decoding
-     */
-    public void decodeRecord(TableSchema ts, byte[] recordBytes) throws IOException {
-        ByteArrayInputStream bs = new ByteArrayInputStream(recordBytes);
-        DataInputStream ds = new DataInputStream(bs);
-
-        for (int i = 0; i < ts.attributes.size(); i++) {
-            AttributeType type = ts.attributes.get(i).type;
-            Object value;
-            switch (type) {
-                case INT:
-                    value = ds.readInt();
-                    break;
-                case DOUBLE:
-                    value = ds.readDouble();
-                    break;
-                case BOOLEAN:
-                    value = ds.readBoolean();
-                    break;
-                case CHAR:
-                case VARCHAR:
-                    StringBuilder sb = new StringBuilder();
-                    for(int j = 0; j < ts.attributes.get(i).length; j++){
-                        sb.append(ds.readChar());
-                    }
-                    value = sb.toString();
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unsupported attribute type: " + type);
-            }
-            rowData.add(value);
-        }
     }
 
     @Override
